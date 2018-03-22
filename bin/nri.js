baf6f8a30aaac7764881ea2d70201f2500c5544f
Rich Remer <rich.remer@gmail.com>
2018-03-22 15:56:48 -0700

    fix timing issue when running remote commands

--- bin/nri.js
+++ bin/nri.js
@@ -3,13 +3,13 @@
 const {assign} = Object;
 const {realpath} = require("fs");
 const {pack: tar} = require("tar-pack");
-const ssh = require("ssh-exec");
 const readpkg = require("fstream-npm");
 const config = require("../lib/config");
-const stringWriter = require("../lib/string-writer");
+const remote = require("../lib/remote");
 
 const {node, script, path, host, user, key} = config.argv(process.argv);
-const sshopts = {host, user, key};
+
+var ssh;
 
 resolveKey()
     .then(makeInstallDir)
@@ -23,74 +23,34 @@ resolveKey()
 
 function resolveKey() {
     return new Promise((resolve, reject) => {
-        if (!sshopts.key) resolve(sshopts.key);
-        else realpath(sshopts.key, (err, path) => {
-            if (!err) sshopts.key = path;
-            resolve(sshopts.key);
-        });
+        if (key) realpath(key, (err, path) => {
+            ssh = err ? remote(host, user) : remote(host, user, path);
+            resolve(path);
+        }); else {
+            ssh = remote(host, user);
+            resolve(key);
+        }
     });
 }
 
 function makeInstallDir() {
     console.info("creating remote working directory");
-
-    return new Promise((resolve, reject) => {
-        const output = stringWriter();
-        const cmd = ssh("mktemp -d", sshopts);
-
-        cmd.pipe(output);
-        cmd.on("error", reject);
-        cmd.on("exit", code => {
-            if (code) reject(new Error("could not create install dir"));
-            else resolve(String(output).trim());
-        });
-    });
+    return ssh("mktemp -d").then(output => output.trim());
 }
 
 function copyPackage(local, remote) {
     console.info(`copying ${local} to ${host}:${remote}`);
-
-    return new Promise((resolve, reject) => {
-        const tarball = tar(readpkg(local));
-        const cmd = ssh(`cd ${remote} && tar xz 2>&1`, sshopts);
-
-        cmd.pipe(process.stdout);
-        cmd.on("error", reject);
-        cmd.on("exit", code => {
-            if (code) reject(new Error(`could not copy package`));
-            else resolve(remote);
-        });
-
-        tarball.pipe(cmd);
-    });
+    const tarball = tar(readpkg(local));
+    return ssh(tarball, `cd ${remote} && tar xz 2>&1`).then(() => remote);
 }
 
 function install(remote) {
     console.info(`running installer from ${remote} on ${host}`);
-
-    return new Promise((resolve, reject) => {
-        const path = `${remote}/package`;
-        const cmd = ssh(`sudo -H npm install -g --prefix ${path} 2>&1`, sshopts);
-
-        cmd.pipe(process.stdout);
-        cmd.on("error", reject);
-        cmd.on("exit", code => {
-            if (code) reject(new Error("could not install package"));
-            else resolve(remote);
-        });
-    });
+    return ssh(`sudo -H npm install -g --prefix ${path} 2>&1`)
+        .then(() => remote);
 }
 
 function clean(remote) {
     console.info(`cleaning up ${host}:${remote}`);
-
-    return new Promise((resolve, reject) => {
-        const cmd = ssh(`rm -fr ${remote}`, sshopts);
-
-        cmd.on("error", reject);
-        cmd.on("exit", code => {
-            if (code) reject(new Error("could not cleanup install dir"));
-            else resolve(remote);
-        });
-    });
+    return ssh(`rm -rf ${remote}`);
 }
